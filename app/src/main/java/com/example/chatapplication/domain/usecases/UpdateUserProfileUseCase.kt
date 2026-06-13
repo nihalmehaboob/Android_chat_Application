598c74751ba8e4ef7ddcccfe018e3d5cbcf6ceb5
Mohamed NIHAL K <115793246+nihalmehaboob@users.noreply.github.com>
2026-06-13 13:21:51 +0530

    last changes

--- app/src/main/java/com/example/chatapplication/domain/usecases/UpdateUserProfileUseCase.kt
+++ app/src/main/java/com/example/chatapplication/domain/usecases/UpdateUserProfileUseCase.kt
@@ -3,10 +3,10 @@ package com.example.chatapplication.domain.usecases
 import com.example.chatapplication.domain.entities.User
 import com.example.chatapplication.domain.entities.repository.UserRepository
 
-class UpdateUserProfileUseCase(
-    private val repository: UserRepository
-) {
-    suspend operator fun invoke(uid: String): User {
-        return repository.updateUserProfile(uid)
-    }
-}
+//class UpdateUserProfileUseCase(
+//    private val repository: UserRepository
+//) {
+//    suspend operator fun invoke(uid: String): User {
+//        return repository.updateUserProfile(uid)
+//    }
+//}
